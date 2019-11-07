3f3ecd7ee482137f9b16dc8696ba1bd705ef69e1
Jeff Swisher <jefftswisher@gmail.com>
2019-11-07 20:32:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
-class Transfer
- 
-end
+class Transfer 
+  attr_accessor :sender, :receiver
+
+  def initialize(sender, receiver)
+    @sender = sender 
+    @receiver = receiver
+    @status = "pending"
+    @amount = 0
+  end 
+end 
