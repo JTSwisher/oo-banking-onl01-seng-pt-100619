66da57740ea850c144f812821f35a39ae97e2b3d
Jeff Swisher <jefftswisher@gmail.com>
2019-11-07 21:04:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,6 +17,7 @@ class Transfer
     if valid? && sender.balance > amount && self.status == "pending"
       sender.balance -= amount
       receiver.balance += amount 
+      self.status = "complete"
     else 
       transfer_rejected
     end  
